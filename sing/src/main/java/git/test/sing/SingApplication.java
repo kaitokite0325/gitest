e7d754675d24714f92e7db29dfbeee854328dcdd
SING <kaito840325@gmail.com>
2023-10-28 17:05:02 +0800

    my first git

---
+++ sing/src/main/java/git/test/sing/SingApplication.java
@@ -0,0 +1,13 @@
+package git.test.sing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingApplication.class, args);
+	}
+
+}
---
+++ sing/src/test/java/git/test/sing/SingApplicationTests.java
@@ -0,0 +1,13 @@
+package git.test.sing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
